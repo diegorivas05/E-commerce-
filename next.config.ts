481570acf17b5b9cbc4401035968709e5b387ef0
diegorivas05 <diegorivas05@hotmail.com>
2026-07-25 09:46:04 -0600

    implement: agregar Toaster de Sonner en RootLayout para notificaciones globales

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
       { protocol: "https", hostname: "images.penguinrandomhouse.com" },
-      { protocol: "https", hostname: "proassetspdlcom.comstatics2.com" },
+      { protocol: "https", hostname: "proassetspdlcom.constatics2.com" },
       { protocol: "https", hostname: "www.librosdelaballena.com" },
       { protocol: "https", hostname: "m.media-amazon.com" },
     ],
